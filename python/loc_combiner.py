29255c351213114351fe07047c2e565f8b49d2ec
Xiaoyuan <50108649+Xiaoyuan-xyz@users.noreply.github.com>
2024-12-11 23:11:28 +0800

    把发动机英文原文写在注释后

--- python/loc_combiner.py
+++ python/loc_combiner.py
@@ -4,17 +4,21 @@ def read_loc(path):
 
     ret = []
     for line in lines:
-        if "=" not in line:
-            continue
-        lines = line.split("//")
-        lines[0] = lines[0].split("=", 1)
-        lines = lines[0] + lines[1:]
-        lines = [line.strip() for line in lines]
-        if lines[1] == "暂未翻译":
-            lines[1] = None
-        if len(lines) == 2:
-            lines.append(None)
-        ret.append(lines)
+        line = line.strip()
+        if line.startswith("//"):
+            triplet = [None, None, line[2:]]
+        else:
+            if '=' not in line:
+                continue
+            triplet = line.split("//")
+            triplet[0] = triplet[0].split("=", 1)
+            triplet = triplet[0] + triplet[1:]
+            triplet = [it.strip() for it in triplet]
+            if triplet[1] == "暂未翻译":
+                triplet[1] = None
+            if len(triplet) == 2:
+                triplet.append(None)
+        ret.append(triplet)
     return ret
 
 
@@ -23,12 +27,15 @@ def write_loc(path, loc, lang):
     tab_str = "        "
     loc_str_list = []
     for line in loc:
-        if line[1] is None:
-            line[1] = "暂未翻译"
-        loc_str = tab_str + line[0] + " = " + line[1]
-        if line[2] is not None:
-            loc_str += " // " + line[2]
-        loc_str += "\n"
+        if line[0] is None:
+            loc_str = tab_str + "// " + line[2] + "\n"
+        else:
+            if line[1] is None:
+                line[1] = "暂未翻译"
+            loc_str = tab_str + line[0] + " = " + line[1]
+            if line[2] is not None:
+                loc_str += " // " + line[2]
+            loc_str += "\n"
         loc_str_list.append(loc_str)
     s += "".join(loc_str_list)
     s += "    }\n}"
@@ -46,8 +53,8 @@ if __name__ == "__main__":
     zh_loc = read_loc(r"./XYZLocalization\Localization\RealismOverhaul\Localization\zh-cn-Engines.cfg")
     en_loc = read_loc(r"./RealismOverhaul\Localization\en-us-Engines.cfg")
     for i in range(len(zh_loc)):
-        zh_loc[i][1] = en_loc[i][1]
-        zh_loc[i][2] = None
+        if zh_loc[i][0] is not None:
+            zh_loc[i][2] = en_loc[i][1]
     write_loc(
         r"./XYZLocalization\Localization\RealismOverhaul\Localization\zh-cn-Engines-temp2.cfg", zh_loc, "zh-cn"
     )
